d9a72a0ea437e5daf550442189bd604858a38487
Micael Rocha <69124656+eu-micaeu@users.noreply.github.com>
2023-07-06 22:02:57 -0300

    by Micael

---
+++ Mercado/src/main/java/com/mycompany/mercado/Conexao.java
@@ -0,0 +1,18 @@
+package com.mycompany.mercado;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Conexao {
+    public Connection getConnection() throws SQLException {
+        
+        String url = "jdbc:mysql://localhost:3306/mercado";
+        String username = "root";
+        String password = "12345678";
+
+        Connection conexao = DriverManager.getConnection(url, username, password);
+        return conexao;
+    }
+    
+}
---
+++ Mercado/src/main/java/com/mycompany/mercado/TesteConexao.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.mercado;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public class TesteConexao {
+    public static void main(String[] args) {
+        Conexao conexao = new Conexao();
+
+        try {
+            Connection connection = conexao.getConnection();
+            System.out.println("Conexão estabelecida com sucesso!");
+            connection.close(); 
+        } catch (SQLException e) {
+            System.out.println("Erro ao estabelecer a conexão: " + e.getMessage());
+        }
+    }
+}
