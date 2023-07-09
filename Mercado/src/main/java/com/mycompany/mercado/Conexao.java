bbf9cb828ee611a71668701936142ebabed14653
Micael Rocha <69124656+eu-micaeu@users.noreply.github.com>
2023-07-09 15:19:21 -0300

    Update Conexao.java

--- Mercado/src/main/java/com/mycompany/mercado/Conexao.java
+++ Mercado/src/main/java/com/mycompany/mercado/Conexao.java
@@ -7,7 +7,7 @@ import java.sql.SQLException;
 public class Conexao {
     public Connection getConnection() throws SQLException {
         String url = "jdbc:mysql://192.168.0.20:3306/mercado";
-        String u = "brena";
+        String u = "micael";
         String p = "12345678";
         Connection conexao = DriverManager.getConnection(url, u, p);
         return conexao;
